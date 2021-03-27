76c5e8584d6c821a7e0f9f977d5c08d69a5dc149
Tedyknight <josbourne4@hotmail.ca>
2021-03-27 15:50:31 -0400

    Bug Fix
    Fixed a bug that stopped hyperlinks from working due to issues with the java script.

--- javascript/jquery.magnific-popup.js
+++ javascript/jquery.magnific-popup.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $(".proj-1").magnificPopup({
+    $(".images").magnificPopup({
         delegate: 'a',
         type:'image',
         gallery: {
@@ -17,78 +17,5 @@ $(document).ready(function() {
           }
       }
     });
-
-    $(".proj-2").magnificPopup({
-        delegate: 'a',
-        type:'image',
-        gallery: {
-            enabled: true
-        },
-
-        zoom: {
-          enabled: true,
-          duration: 300, 
-          easing: 'ease-in-out',
-          opener: function(openerElement) {
-        
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
-          }
-      }
-    });
-
-    $(".proj-3").magnificPopup({
-        delegate: 'a',
-        type:'image',
-        gallery: {
-            enabled: true
-        },
-
-        zoom: {
-          enabled: true,
-          duration: 300, 
-          easing: 'ease-in-out',
-          opener: function(openerElement) {
-        
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
-          }
-      }
-    });
-
-    $(".proj-4").magnificPopup({
-        delegate: 'a',
-        type:'image',
-        gallery: {
-            enabled: true
-        },
-
-        zoom: {
-          enabled: true,
-          duration: 300, 
-          easing: 'ease-in-out',
-          opener: function(openerElement) {
-        
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
-          }
-      }
-    });
-
-    $(".proj-5").magnificPopup({
-        delegate: 'a',
-        type:'image',
-        gallery: {
-            enabled: true
-        },
-
-        zoom: {
-          enabled: true,
-          duration: 300, 
-          easing: 'ease-in-out',
-          opener: function(openerElement) {
-        
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
-          }
-      }
-    });
-
     
   });
